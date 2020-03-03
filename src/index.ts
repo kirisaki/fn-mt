d8aa368bb4a062b4ae78c56552fd5a3144a01a41
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-03-03 22:27:09 +0900

    feat: Add nextRange

--- src/index.ts
+++ src/index.ts
@@ -25,24 +25,24 @@ const WHOLE_MASK = parseInt([...Array(W)].map(() => '1').reduce((x, y) => x + y)
 const UPPER_MASK = parseInt([...Array(W - R)].map(() => '1').reduce((x, y) => x + y).concat([...Array(R)].map(() => '0').reduce((x, y) => x + y)), 2)
 const LOWER_MASK = parseInt([...Array(W - R)].map(() => '0').reduce((x, y) => x + y).concat([...Array(R)].map(() => '1').reduce((x, y) => x + y)), 2)
 
-export type RndGen = {
+export type RandGen = {
   i: number;
   x: number[];
 }
 
-export const newGen = (seed: number): RndGen => {
-  let state: RndGen = {i: 0, x: [seed & WHOLE_MASK]}
+export const newRandGen = (seed: number): RandGen => {
+  let state: RandGen = {i: 0, x: [seed & WHOLE_MASK]}
   for (let j = 1; j < N; j++){
     state.x[j] = (1812433253 * (state.x[j-1] ^ (state.x[j-1] >> 30)) + j) & WHOLE_MASK
   }
   return state
 }
 
-export const randInt = (state: RndGen): [number, RndGen] => {
+export const randNext = (state: RandGen): [number, RandGen] => {
   const newi = (state.i + 1) % N
   const z = state.x[state.i] & UPPER_MASK | state.x[newi] & LOWER_MASK
   const newx = [...state.x, state.x[(state.i + M) % N] ^ (z >> 1) ^ ((z & 1) == 0 ? 0 : A)]
-  const newState: RndGen = {i: newi, x:newx}
+  const newState: RandGen = {i: newi, x:newx}
 
   let y = newState.x[newState.i]
   y = y ^ (y >> U)
@@ -51,4 +51,22 @@ export const randInt = (state: RndGen): [number, RndGen] => {
   y = y ^ (y >> L)
 
   return [y, newState]
+}
+
+export const randRange = (min: number, sup: number, state: RandGen): [number, RandGen] => {
+  if (!(0 < sup && sup < 0x100000000)){
+    const [n, newState] = randNext(state)
+    return [n + min, newState]
+  }
+  if ((sup & (~sup + 1)) == sup){
+    const [n, newState] = randNext(state)
+    return [(sup - 1) & n + 1, newState]
+  }
+  let [n, newState] = randNext(state)
+  while(sup > 4294967296 - (n - (n %= sup))){
+    const [n_, newState_] = randNext(newState)
+    n = n_
+    newState = newState_
+  }
+  return [n + min, newState]
 }
